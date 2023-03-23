4f9b9aa4799741907c537fd263572c230b94f6e7
Bikingman <daniel.patterson87@gmail.com>
2023-03-23 06:48:36 -0400

    national run

--- process_files/01_extract_osm.py
+++ process_files/01_extract_osm.py
@@ -7,16 +7,62 @@ import time
 
 output_path = r'/src/data'
 
-files = [
-  #     'colorado', 
-  #    'florida',
-  #    'oklahoma',
-  #    'texas',  
-  #    'virgina',
-      'washington'  
- ]
 
-#files = ['washington_dc']
+files = [
+    "Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "District of Columbia",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Puerto Rico",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "United States Virgin Islands",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming"
+]
 
 fclass  = [          
     'motorway', 'motorway_link',
