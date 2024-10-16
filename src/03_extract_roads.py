1df2d76eb43ca3bb92743ac42a4bcfa7567e6b16
Bikingman <daniel.patterson87@gmail.com>
2024-10-15 22:22:34 -0400

    streamline example

--- src/03_extract_roads.py
+++ src/03_extract_roads.py
@@ -2,16 +2,26 @@
 import os 
 import geopandas as gpd
 import pandas as pd
-
-output_path = r'/src/data'
+output_path = r'/Users/danielpatterson/Documents/output'
 values = ['Bike Lane', 'Buffered Bike Lane', 'Protected Bike Lane', 'Shared Use Path', 'Cycleway']
 file_BK = gpd.GeoDataFrame()
 
-files = 'states'
+files = [
+    os.path.abspath(os.path.join(output_path, x))
+    for x in os.listdir(output_path)
+    if os.path.isfile(os.path.join(output_path, x))
+]
 
 for i in files:
-    file = gpd.read_file(os.path.join(output_path, 'shp', 'osm', i + '_ways.shp'))
-    bike_infra = file[file['min_bk_inf'].isin(values)]
+    if "_ways" not in i:
+        continue
+    if ".shp" not in i:
+        continue
+    print('Working on: ' + i)
+    file = gpd.read_file(i)
+    bike_infra = file[file['min_bk_inf'].notna()]
+    bike_infra['state'] = os.path.splitext(os.path.basename(i.replace(' ', '_').replace('_ways', '')))[0]
     file_BK = pd.concat([file_BK, bike_infra])
 
-file_BK.to_file('zzfile_BK.shp')
+
+file_BK.to_file('bike_infrastrucure_data_usa.shp')
